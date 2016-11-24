f22659005e848862b361c8ee8610c9de2ae1f231
katliang <katliang@outlook.com>
2016-11-24 01:08:51 +0000

    reorganized js

---
+++ static/js/semantic.js
@@ -0,0 +1,30 @@
+// Semantic UI
+
+$(document)
+    .ready(function() {
+
+        // fix menu when passed
+        $('.masthead')
+            .visibility({
+                once: false,
+                onBottomPassed: function() {
+                    $('.fixed.menu').transition('fade in');
+                },
+                onBottomPassedReverse: function() {
+                    $('.fixed.menu').transition('fade out');
+                }
+            });
+
+        // create sidebar and attach to menu open
+        $('.ui.sidebar')
+            .sidebar('attach events', '.toc.item');
+
+        // customizes checkboxes
+        $('.ui.checkbox')
+            .checkbox();
+
+        // customizes dropdown menu
+        $('#diet')
+            .dropdown();
+
+    });
